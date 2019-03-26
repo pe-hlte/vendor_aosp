3a151edbb6ff644568748fc2f088bb95ef19c1c1
Joshua Primero <jprimero155@gmail.com>
2019-03-26 08:20:54 +0000

    add back some soong
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,18 +6,48 @@ type Product_variables struct {
 	Bootloader_message_offset struct {
 		Cflags []string
 	}
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
+	Should_skip_waiting_for_qsee struct {
+		Cflags []string
+	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
+	Uses_nvidia_enhancements struct {
+		Cppflags []string
+	}
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Java_Source_Overlays *string `json:",omitempty"`
+	Should_skip_waiting_for_qsee  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_nvidia_enhancements  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
